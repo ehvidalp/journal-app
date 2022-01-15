c649f1c6d4a80fbcd9f28f7f102994018f1c56c2
ehvidalp <ehvidalp@gmail.com>
2022-01-14 19:30:41 -0600

    🎨 add bootstrap and sass

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png" alt="Vue logo" />
+
+    <h1>Hola mundo</h1>
+    <button class="btn btn-primary">Primary</button>
+    <button>Secondary</button>
+    <button>Success</button>
   </div>
 </template>
