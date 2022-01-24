56f2a5472c48cba974b4c6f4efcc0ab10fbbf400
ehvidalp <ehvidalp@gmail.com>
2022-01-24 17:07:55 -0600

    💬 add text in object entries

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   isLoading: true,
-  entries: [1, 2, 3, 4],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti, consectetur, deleniti veritatis, doloremque ullam praesentium vel sit nam mollitia laboriosam. Sint voluptates distinctio quis eligendi explicabo eius, quisquam expedita?',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString,
+      text: 'Sed corrupti, consectetur, deleniti veritatis, doloremque ullam praesentium vel sit nam mollitia laboriosam. Sint voluptates distinctio quis eligendi explicabo eius, quisquam expedita?',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString,
+      text: 'Doloremque ullam praesentium vel sit nam mollitia laboriosam. Sint voluptates distinctio quis eligendi explicabo eius, quisquam expedita?',
+      picture: null,
+    },
+  ],
 });
