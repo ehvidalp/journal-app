c63588cef0bb3d7d0c94f1c1e0c9b962e5052d51
ehvidalp <ehvidalp@gmail.com>
2022-01-28 19:14:07 -0600

    ✨ add get entry by id

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -11,4 +11,13 @@ export const getEntriesByTerm =
       entry.text.toLowerCase().includes(term.toLowerCase()),
     );
   };
-export const getEntryById = (/*state */) => {};
+export const getEntryById =
+  (state) =>
+  (id = '') => {
+    const entry = state.entries.find((entry) => entry.id === id);
+
+    if (!entry) {
+      return;
+    }
+    return { ...entry };
+  };
