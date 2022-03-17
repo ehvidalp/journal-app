d8d38858c880ad39ac3c62c82274dda40f6f6ecb
ehvidalp <ehvidalp@gmail.com>
2022-03-16 20:05:12 -0600

    ✨Update entry with url

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-sucess fs-3 fw-bold">{{ getDate.day }}</span>
-      <span class="mx-1 fs-3">{{ getDate.month }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ getDate.yearDay }}</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-sucess fs-3 fw-bold">{{ getDate.day }}</span>
+        <span class="mx-1 fs-3">{{ getDate.month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ getDate.yearDay }}</span>
+      </div>
+
+      <div>
+        <button type="button" class="btn btn-danger mx-2">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+
+        <button type="button" class="btn btn-primary mx-2">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
     </div>
 
-    <div>
-      <button type="button" class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
+    <hr />
 
-      <button type="button" class="btn btn-primary mx-2">
-        Subir foto
-        <i class="fa fa-upload"></i>
-      </button>
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea v-model="entry.text" name="" placeholder="¿Qué sucedió hoy?"></textarea>
     </div>
-  </div>
-
-  <hr />
-
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea v-model="entry.text" name="" placeholder="¿Qué sucedió hoy?"></textarea>
-  </div>
-  <Fab icon="fa-save" />
-  <img
-    src="https://img.vixdata.io/pd/jpg-large/es/sites/default/files/btg/cine.batanga.com/files/cual-es-el-mejor-hulk-de-la-historia-del-cine.jpg"
-    alt="entry-picture"
-    class="img-thumbnail"
-  />
+    <Fab icon="fa-save" />
+    <img
+      src="https://img.vixdata.io/pd/jpg-large/es/sites/default/files/btg/cine.batanga.com/files/cual-es-el-mejor-hulk-de-la-historia-del-cine.jpg"
+      alt="entry-picture"
+      class="img-thumbnail"
+    />
+  </template>
 </template>
 
 <script>
@@ -55,6 +57,11 @@ export default {
       entry: null,
     };
   },
+  watch: {
+    id() {
+      this.loadEntry();
+    },
+  },
   computed: {
     ...mapGetters('journal', ['getEntryById']),
     getDate() {
@@ -66,7 +73,7 @@ export default {
       const entry = this.getEntryById(this.id);
 
       if (!entry) {
-        this.$router.push({ name: 'no-entry' });
+        return this.$router.push({ name: 'no-entry' });
       }
 
       this.entry = entry;
